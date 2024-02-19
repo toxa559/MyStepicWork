aa5bc2e042138a40c7c1cb503d051e6e5296ab31
toxa559 <toxa559@gmail.com>
2024-02-19 16:57:51 +0300

    3.7

--- app/src/main/java/com/example/mystepicwork/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mystepicwork/presentation/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.mystepicwork.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.mystepicwork.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivity", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
--- app/src/main/java/com/example/mystepicwork/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/mystepicwork/presentation/MainViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.mystepicwork.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mystepicwork.data.ShopListRepositoryImpl
 import com.example.mystepicwork.domain.DeleteShopListUseCase
 import com.example.mystepicwork.domain.EditShopListUseCase
 import com.example.mystepicwork.domain.GetShopListUseCase
+import com.example.mystepicwork.domain.ShopItem
 
 class MainViewModel: ViewModel() {
 
@@ -14,6 +16,22 @@ class MainViewModel: ViewModel() {
     private val deleteShopListUseCase = DeleteShopListUseCase(repository)
     private val editShopListUseCase = EditShopListUseCase(repository)
 
-    
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopListUseCase.deleteShopList(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopListUseCase.editShopList(newItem)
+        getShopList()
+    }
 
 }
